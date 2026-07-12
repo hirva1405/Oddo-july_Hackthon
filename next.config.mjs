a3e05a1b2c54dd22d83613d4905e0094fda89913
Hirva Trivedi <160419035+hirva1405@users.noreply.github.com>
2026-07-12 18:05:30 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-export default {
-  reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ["better-sqlite3"],
-    serverActions: { bodySizeLimit: "8mb" },
-  },
-};
